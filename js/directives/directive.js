9f2c980ada443d5209cb2f9bd4bc797bbf05f67f
durgeshdbydx <durgesh.singh@kelltontech.com>
2016-12-16 19:05:58 +0530

    souce formatting

--- js/directives/directive.js
+++ js/directives/directive.js
@@ -1,48 +1,53 @@
 // Directive for generic chart, pass in chart options
-	app.directive('hcChart', function () {
+app
+		.directive('hcChart', function() {
 			return {
-				restrict: 'E',
-				template: '<div></div>',
-				scope: {
-					options: '='
+				restrict : 'E',
+				template : '<div></div>',
+				scope : {
+					options : '='
 				},
-				link: function (scope, element) {
+				link : function(scope, element) {
 					Highcharts.chart(element[0], scope.options);
 				}
 			};
 		})
-		// Directive for pie charts, pass in title and data only    
-		.directive('hcPieChart', function () {
-			return {
-				restrict: 'E',
-				template: '<div></div>',
-				scope: {
-					title: '@',
-					data: '='
-				},
-				link: function (scope, element) {
-					Highcharts.chart(element[0], {
-						chart: {
-							type: 'pie'
+		// Directive for pie charts, pass in title and data only
+		.directive(
+				'hcPieChart',
+				function() {
+					return {
+						restrict : 'E',
+						template : '<div></div>',
+						scope : {
+							title : '@',
+							data : '='
 						},
-						title: {
-							text: scope.title
-						},
-						plotOptions: {
-							pie: {
-								allowPointSelect: true,
-								cursor: 'pointer',
-								dataLabels: {
-									enabled: true,
-									format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-								}
-							}
-						},
-						series: [{
-							data: scope.data
-						}]
-					});
-				}
-			};
-	});
-		
+						link : function(scope, element) {
+							Highcharts
+									.chart(
+											element[0],
+											{
+												chart : {
+													type : 'pie'
+												},
+												title : {
+													text : scope.title
+												},
+												plotOptions : {
+													pie : {
+														allowPointSelect : true,
+														cursor : 'pointer',
+														dataLabels : {
+															enabled : true,
+															format : '<b>{point.name}</b>: {point.percentage:.1f} %'
+														}
+													}
+												},
+												series : [ {
+													data : scope.data
+												} ]
+											});
+						}
+					};
+				});
