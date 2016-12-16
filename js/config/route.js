090d0007d8335820ba1cf4215f696ff9fd12425a
durgeshdbydx <durgesh.singh@kelltontech.com>
2016-12-16 18:58:22 +0530

    Seprate file

---
+++ js/config/route.js
@@ -0,0 +1,24 @@
+// Routing Information 		
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/demo', {
+    templateUrl : 'pages/demo.html',
+    controller  : 'DemoController'
+  })
+  .when('/secon-demo', {
+    templateUrl : 'pages/homebk.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/about', {
+    templateUrl : 'pages/about.html',
+    controller  : 'AboutController'
+  })
+
+  .otherwise({redirectTo: '/'});
+});
---
+++ js/controllers/controller.js
@@ -0,0 +1,80 @@
+app.controller('HomeController', function($scope) { 
+  $scope.message = 'Hello from HomeController';
+  $scope.toggleField = function() { 
+        if($scope.isWellnessOpen = !$scope.isWellnessOpen){
+			$scope.isWellnessOpen = true;
+		}else{
+			$scope.isWellnessOpen = false;
+			$scope.isthirdOpen = false;
+			$scope.isfourthOpen = false;
+			$scope.isScreen2 = false;
+			$scope.isScreen3 = false;
+			
+		}
+      };
+	   $scope.toggleWell = function() { 
+        if($scope.isthirdOpen = !$scope.isthirdOpen){
+			$scope.isthirdOpen = true;
+		}else{
+			$scope.isthirdOpen = false;
+			$scope.isfourthOpen = false;
+			$scope.isScreen2 = false;
+			$scope.isScreen3 = false;
+         }
+      };
+	   $scope.toggleSensor = function() { 
+        if($scope.isfourthOpen = !$scope.isfourthOpen){
+			$scope.isfourthOpen = true;
+		}else{
+			
+			$scope.isfourthOpen = false;
+			$scope.isScreen3 = false;
+         }
+      };
+	  
+	   $scope.chartOptions = {
+                    title: {
+                        text: 'Temperature data'
+                    },
+                    xAxis: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    },
+
+                    series: [{
+                        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+                    }]
+                };
+
+                // Sample data for pie chart
+                $scope.pieData = [{
+                        name: "Microsoft Internet Explorer",
+                        y: 56.33
+                    }, {
+                        name: "Chrome",
+                        y: 24.03,
+                        sliced: true,
+                        selected: true
+                    }, {
+                        name: "Firefox",
+                        y: 10.38
+                    }, {
+                        name: "Safari",
+                        y: 4.77
+                    }, {
+                        name: "Opera",
+                        y: 0.91
+                    }, {
+                        name: "Proprietary or Undetectable",
+                        y: 0.2
+                }]
+	  
+});
+
+app.controller('DemoController', function($scope) {
+  $scope.message = 'Demo Coming Soon';
+});
+
+app.controller('AboutController', function($scope) {
+  $scope.message = 'About Coming Soon';
+});
